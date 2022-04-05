4eaeb90453f130c9c7557cd79f7948caf77cd490
R-graft <trianglezexx@rambler.ru>
2022-04-05 16:15:14 +0300

    Update Books.cs

--- Assets/Script/Books.cs
+++ Assets/Script/Books.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class Books : MonoBehaviour
 {
     public GameObject[] book;
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 6b06ecc (third)
     public int aa;
     void Start()
     {
